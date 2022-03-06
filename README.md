382642f6ba4b6bc0da2e264be228879e6510fc58
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-06 16:24:51 -0600

    Client.js; amm'd- deleteEmployee func to track employee budget when deleted and update in DOM. Stretch Complete. Index.html; add: removed unused class id's, Style.css; add: font for all text, added table f/ bootstrap, adjust colors, font size

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+Weekend Jquery Salary Calculator
 
 ## Description
+_Duration: Weekend Homework Assignment_
 
-Your project description goes here. What problem did you solve? How did you solve it?
+I created an application that collects _employee first name, last name, ID number, job title, annual salary_ and tracks a monthly salary budget based on annual salary for all employees.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+Once user input is submitted using a submit button, it collects the information, stores the information to calculate monthly costs and displays that information to the DOM. It calculates monthly costs and displays to DOM. If the total monthly cost exceeds $20,000 a red color warning will apply to the total monthly cost to alert the user.
+
+Employees can be deleted from the DOM using a delete button.
+
+**remove that Employee's salary from the reported total.
+
+## Screen Shot
+
+Include one or two screen shots of your project here (optional). Remove if unused.
+
+### Prerequisites
+
+Link to software that is required to install the app (e.g. node).
+
+- [Jquery.js](https://jquery.com/)
+- [[bootstrap.css](https://jquery.com/)
+
+## Usage
+
+1. Enter employee information into information fields.
+2. Press 'Submit' to add employees to the table below.
+3. Press 'Delete' to remove an employee from the Employee List.
+4. Monthly budget should not exceed $20000. If monthly budget is exceeded the total monthly salary costs will display with a red background to alert the user.  
+
+## Built With
+
+-Javascript
+-Jquery
+-Css
+-Html
+
+## Acknowledgement
+Thanks to my dog, Tank cause he's awesome. 
+
+## Support
+If you have suggestions or issues, please email me at [joshua.j.salsbury@gmail.com](www.google.com)
--- client.js
+++ client.js
@@ -1,133 +1,131 @@
+
 //Log'd to show javascript sourced.
 console.log('js loaded!');
 //Jquery call to document initiate starting output to DOM.
 $(document).ready(handleReady);
 
-
 //Created function to handle ready tasks.
 function handleReady() {
-//Logged to show jquery sourced.
+    //Logged to show jquery sourced.
     console.log('jquery loaded!');
-//jquery call to button element. Should call functions on click.
-     $('#submitBtn').on('click', handleClick);
-     $('#submitBtn').on('click', addEmployee);
-//Jquery call to button element. Should call button and function on click.
-     $('#employeeListId').on('click', '.deleteBtn', deleteEmployee);
-}//End of handleReady.
+    //jquery call to button element. Should call functions on click.
+    $('#submitBtn').on('click', handleClick);
+    $('#submitBtn').on('click', addEmployee);
+    //Jquery call to button element. Should call button and function on click.
+    $('#employeeListId').on('click', '.deleteBtn', deleteEmployee);
+}//end of handleReady
 
 //Created handleClick function to manage my click events.
 function handleClick() {
     console.log('clicked!');
 }
 
-//The application should have an input form that collects;
-//employee first name, last name, ID number, job title, annual salary (declare object to store)
-//1. create input field in html
-//2. display to DOM
-//3. create function to meet input requirements.
-
 //Created a global variable to collect employee input data in an array.
 let ourEmployees = [];
-////Created a global variable to collect all salary data in an. Set it's value to match the value used for demonstration in the DOM. 
-let monthlySalary = 19999;
+//Created a global variable to collect all salary data in an. Set it's value to match the value used for demonstration in the DOM. 
+let monthlySalary = 0;
 
 //Created function to add user input into ourEmployees array.
 function addEmployee() {
-//Console log'd to show function initiation in console.
+    //Console log'd to show function initiation in console.
     console.log('in addEmployee');
-//Declared variables for input items and used Jquery call to element id and set the value.
+    //Declared variables for input items and used Jquery call to element id and set the value.
     let firstName = $('#fNameId').val();
     let lastName = $('#lNameId').val();
     let id = $('#idId').val();
     let title = $('#titleId').val();
     let salary = $('#salaryId').val();
-//Created object to name value pairs to contain employee input information and create employee obj with name value pairs to receive 
-//input for every employee added to the ourEmployee Array .
+    //Created object to name value pairs to contain employee input information and create employee obj with name value pairs to receive 
+    //input for every employee added to the ourEmployee Array .
     const employeeObj = {
         firstName: firstName,
         lastName: lastName,
         id: id,
         title: title,
-        salary: salary, 
-    
+        salary: salary
     }
 
-//Added employeeObj values to ourEmployee array.
-ourEmployees.push(employeeObj);
+    //Added employeeObj values to ourEmployee array.
+    ourEmployees.push(employeeObj);
 
-//Call to displayEmployee function to initiate in addEmployee function.
-displayEmployees();
-//Call to salaryTracker function to initiate in addEmployee function.
-salaryTracker();
+    //Jquery call to input elements and cleared value.
+    // $(`input`).val('');
+    $('#fNameId').val('');
+    $('#lNameId').val('');
+    $('#idId').val('');
+    $('#titleId').val('');
+    $('#salaryId').val('');
 
-//Jquery call to input elements and cleared value.
-$(`input`).val('');
-    // $('#fNameId').val('');
-    // $('#lNameId').val('');
-    // $('#idId').val('');
-    // $('#titleId').val('');
-    // $('#salaryId').val('');
+    //Call to displayEmployee function to initiate in addEmployee function.
+    displayEmployees();
+    //Call to salaryTracker function to initiate in addEmployee function.
+    salaryTracker();
 
-//Log'd to see employees as they are added in the console.
-console.log('Number of Employees:', ourEmployees.length);
-//Log'd to see ourEmployee array in the console.
-console.log(ourEmployees);
-}//end add employee
+    //Log'd to see employees as they are added in the console.
+    console.log('Number of Employees:', ourEmployees.length);
+    //Log'd to see ourEmployee array in the console.
+    console.log(ourEmployees);
+}//end addEmployee
 
 //Created function to display employees added to ourEmployee array.
-function displayEmployees () {
-//Used a for loop to loop through each employee in the ourEmployee array.
-    for ( let employee of ourEmployees){
-//Jquery call to target element id and appended employee information into list.
-//Added delete button element to be created upon every new employee object creation and should display to DOM.  
-        $( '#employeeListId' ).append(`
+function displayEmployees() {
+    //Jquery call to element id to clear previously pushed data when an employee was previously added.
+    $("#employeeListId").empty();
+    //Used a for loop to loop through each employee in the ourEmployee array.
+    for (let employee of ourEmployees) {
+        //Jquery call to target element id and appended employee information into list.
+        //Added delete button element to be created upon every new employee object and should display to DOM.  
+        $('#employeeListId').append(`
         <tr>
             <td>${employee.firstName}</td> 
             <td>${employee.lastName}</td>
             <td>${employee.id}</td>  
             <td>${employee.title}</td>  
             <td>${employee.salary}</td>
-            <td><button class="deleteBtn">Delete</button></td>
+            <td><button class="deleteBtn" data-object="${employee.salary}">Delete</button></td>
         </tr> 
         `)
-    }  
-}//End displayEmployees Function.
+    }
+}//end displayEmployees
 
 //Create a function to gather the total monthly cost for employ salaries in the ourEmployees array to display to DOM.
 //Call in addEmployee function, should initiate onclick.
 function salaryTracker() {
-     for ( employee of ourEmployees ){
-//For each employee, add up the total value of annual salaries and divide by twelve to find monthly salary 
-//using the math.floor method to return a whole number and avoid repeating remainders. 
-      monthlySalary += Math.floor (Number(employee.salary)/12);
-     }
-//Jquery call to target salary id and empty text.
-     $('#salaryTrackerId').empty();
-//Jquery call to target salary id and append price, display to DOM.
-     $('#salaryTrackerId').append(monthlySalary);
-//Call to budgetWarning function to initiate in addEmployee function.
-     budgetWarning();
+    monthlySalary = 0;
+    for (employee of ourEmployees) {
+        //For each employee, add up the total value of annual salaries and divide by twelve to find monthly salary 
+        //using the math.floor method to return a whole number and avoid repeating remainders. 
+        monthlySalary += Number(employee.salary) / 12;
     }
+    //Jquery call to target salary id and empty text.
+    $('#salaryTrackerId').empty();
+    //Jquery call to target salary id and append price, display to DOM.
+    $('#salaryTrackerId').append(Math.floor(monthlySalary));
+    //Call to budgetWarning function to initiate in addEmployee function.
+    budgetWarning();
+}//end salaryTracker
 
 //Created function to delete employee from the DOM.
 function deleteEmployee() {
-//Jquery 'this' call to target tr element when initiated by click event, and remove the date from the DOM. 
+    //When employee is deletes, access salary data and removed from DOM
+    let deleteSalary = $(this).data('object');
+    monthlySalary = (monthlySalary - deleteSalary / 12);
+    //Jquery call to update salary to DOM when employee is deleted.
+    $('#salaryTrackerId').text(Math.floor(monthlySalary));
+    //Jquery 'this' call to target tr element when initiated by click event, and remove the date from the DOM. 
     $(this).closest('tr').remove();
-//Log'd to see deleteEmployee function initiation in the console.
+    //Log'd to see deleteEmployee function initiation in the console.
     console.log('Delete');
-    
-}
+    //Call to budget warning function to initiate.
+    budgetWarning();
+}//end deleteEmployee
 
-//Created a function to process budget data
+//Created a function to turn monthly salary budget displayed to the DOM red
 function budgetWarning() {
-//Used an if statement to declare that if monthly salary is greater than $20,000 initiate a warning that 
-//turns that add css styling to the element id.
+    //Used an if statement to declare that if monthly salary is greater than $20,000 initiate a warning that 
+    //turns that add css styling to the element id.
     if (monthlySalary > 20000) {
-//Jquery call to an element id and added styling input from css by adding a class name to be used in CSS. 
-$("#salaryTrackerId").toggleClass('warning');
+        //Jquery call to an element id and added styling input from css by adding a class name to be used in CSS. 
+        $("#salaryTrackerId").toggleClass('warning');
     }
-}
-
-
-
-
+}//end budgetWarning
